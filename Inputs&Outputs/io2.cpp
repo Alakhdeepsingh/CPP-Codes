8b34114ee4de646200ebe25a7102986b5b78053f
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2020-10-31 02:19:42 +0530

    Cpp codes

---
+++ Inputs&Outputs/io2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char ch;
+cout<<"press a character and press return"<<endl;
+cin>>ch;
+cout<<ch;
+    return 0;
+}
+
+
---
+++ Inputs&Outputs/io3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float b;
+double c;
+cin>>b;
+cout<<b<<endl;
+cin>>c;
+cout<<c<<endl;
+    return 0;
+}
+
